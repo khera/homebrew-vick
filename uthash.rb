be57ed0e223161f8d4d5e3c5178fa6da6baeb7ed
Vick Khera <vivek@khera.org>
2013-06-25 14:48:50 -0400

    found link to versioned release of uthash, so using it so checksum doesn't change from under us again.

--- uthash.rb
+++ uthash.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Uthash < Formula
   homepage 'http://troydhanson.github.io/uthash/'
-  url 'https://github.com/troydhanson/uthash/archive/master.zip'
+  url 'https://github.com/troydhanson/uthash/archive/v1.9.8.zip'
   version '1.9.8'
-#  sha1 '578cb3ff16241ecbbf4ce015e6ba9652d90c1f4b'
+  sha1 '53e05e0c4dea79fc736fcefbcbec5c4d456e9a78'
 
   def install
     include.install Dir['src/*.h']
