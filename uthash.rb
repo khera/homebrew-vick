15a1c9a88f98c643112d77b4615b6cdf2188c8a6
Vick Khera <vivek@khera.org>
2013-06-25 14:43:06 -0400

    remove checksum from uthash. there is no "release" and it pulls HEAD from git.

--- uthash.rb
+++ uthash.rb
@@ -4,7 +4,7 @@ class Uthash < Formula
   homepage 'http://troydhanson.github.io/uthash/'
   url 'https://github.com/troydhanson/uthash/archive/master.zip'
   version '1.9.8'
-  sha1 '578cb3ff16241ecbbf4ce015e6ba9652d90c1f4b'
+#  sha1 '578cb3ff16241ecbbf4ce015e6ba9652d90c1f4b'
 
   def install
     include.install Dir['src/*.h']
