7f327342417f37ddf4d9ebe29b5467d61b8c4944
Vick Khera <vivek@khera.org>
2013-08-23 08:43:35 -0400

    updated sha1

--- uthash.rb
+++ uthash.rb
@@ -4,7 +4,7 @@ class Uthash < Formula
   homepage 'http://troydhanson.github.io/uthash/'
   url 'https://github.com/troydhanson/uthash/archive/v1.9.8.zip'
   version '1.9.8'
-  sha1 '53e05e0c4dea79fc736fcefbcbec5c4d456e9a78'
+  sha1 '2a6c33c0cd5f7c9932297ed12e2c763636465135'
 
   def install
     include.install Dir['src/*.h']
